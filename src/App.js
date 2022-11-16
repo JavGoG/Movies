322cb7135b983e61b6a567d778463d116672a8cc
JavGog <javierggjavier@gmail.com>
2022-11-16 00:11:02 +0000

    Adding comments to clarify the code

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import { useEffect } from 'react';
 
 const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=d39fd0c0"
 
-// key d39fd0c0
 function App() {
 
+  // Front-End connects with Back-End API
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`)
     const data = await response.json();
-    console.log(data);
+    // The API has got the Search property field
+    // which holds the values of our API request
+    console.log(data.Search);
 
   }
+  // searchMovies are called once when the page is loaded
   useEffect(() => {
     searchMovies('The Godfather');
   }, [])
